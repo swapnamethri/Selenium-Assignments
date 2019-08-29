c9574f8bb3f733d7c873b57668c42ce9829a0a8d
“Swapna” <swapna.methri@gmail.com>
2019-08-29 18:02:23 -0400

    Added Assignment 1 ad 2 , still working on 3

---
+++ Aug 27 work/SeleniumTitleDownload.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace Aug_27_work
+{
+    class SeleniumTitleDownload
+    {
+        ChromeDriver driver;
+
+
+        [OneTimeSetUp]
+        public void Openbrowser()
+        {
+          
+           driver = new ChromeDriver();
+            //Maximize Browser
+            driver.Manage().Window.Maximize();
+            //Navigate to Web page
+            driver.Url = "https://www.seleniumhq.org/download";
+        }
+
+        [Test]
+
+        public void VerifyTitle()
+        {
+            string expectedTitle = "Downloads";
+            string actualTitle = driver.Title;
+            Assert.AreEqual(expectedTitle, actualTitle);
+
+        }
+
+        public void VerifyText()
+        {
+            string expectedtext = "Selenium Client & WebDriver Language Bindings";
+            string actualtext = driver.FindElementByCssSelector("#mainContent > a:nth-child(11) > h3").Text;
+            Assert.AreEqual(expectedtext, actualtext);
+        }
+
+        [OneTimeTearDown]
+
+        public void Closebrowser()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ Aug 27 work/VerifySeleniumsiteTitle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace Aug_27_work
+{
+    class VerifySeleniumsiteTitle
+    {
+        ChromeDriver driver;
+
+
+        [OneTimeSetUp]
+        public void Openbrowser()
+        {
+
+            driver = new ChromeDriver();
+            //Maximize Browser
+            driver.Manage().Window.Maximize();
+            //Navigate to Web page
+            driver.Url = "https://www.seleniumhq.org/";
+        }
+
+        [Test]
+
+        public void VerifyTitle()
+        {
+            string expectedTitle = "Selenium - Web Browser Automation";
+            string actualTitle = driver.Title;
+            Assert.AreEqual(expectedTitle, actualTitle);
+
+        }
+        [OneTimeTearDown]
+        public void Closebrowser()
+        {
+            driver.Close();
+        }
+
+    }
+
+}
